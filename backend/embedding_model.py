17234a384c29b1f350d71fb649228af89c268f5b
DEVANSHU-KALI <devanshu9666@gmail.com>
2026-05-25 17:28:24 +0530

    made some changes in embedding_model.py

--- backend/embedding_model.py
+++ backend/embedding_model.py
@@ -4,3 +4,6 @@ from sentence_transformers import SentenceTransformer
 embedding_model = SentenceTransformer(
     "all-MiniLM-L6-v2"
 )
+
+# this model has 384 dimensions, and you can go with other models too. and as mentioned, all the explanation of scripts will be available in the explanations/backend folder.
+
